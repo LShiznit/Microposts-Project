4b84dc9752b7b117c526aa9c6037abb6c1da65c6
Leo <lmaximilians@gmail.com>
2022-07-20 13:45:39 +0200

    missing edit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,82 @@
-import { http } from './http'
+import { http } from './http.js'
+import { ui } from './ui.js'
 
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts)
 
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost)
+
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePosts)
+
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit)
+
+// Get Posts
 function getPosts() {
-  http.get('')
+  http
+    .get('http://localhost:3000/posts')
+    .then((data) => ui.showPosts(data))
+    .catch((err) => console.log(err))
+}
+
+// Submit Post
+function submitPost() {
+  const title = document.querySelector('#title').value
+  const body = document.querySelector('#body').value
+
+  const data = {
+    title,
+    body,
+  }
+
+  // Create Post
+  http
+    .post('http://localhost:3000/posts', data)
+    .then((data) => {
+      ui.showAlert('Post Added', 'alert alert-success')
+      ui.clearFields()
+      getPosts()
+    })
+    .catch((err) => console.log(err))
+}
+
+// Delete Posts
+function deletePosts(e) {
+  e.preventDefault()
+  if (e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id
+    if (confirm('Are you sure?')) {
+      http
+        .delete(`http://localhost:3000/posts/${id}`)
+        .then((data) => {
+          ui.showAlert('Posts Removed', 'alert alert-success')
+          getPosts()
+        })
+        .catch((err) => console.log(err))
+    }
+  }
+}
+
+// Enable Edit State
+function enableEdit(e) {
+  if (e.target.parentElement.classList.contains('edit')) {
+    const id = e.target.parentElement.dataset.id
+    const title =
+      e.target.parentElement.previousElementSibling.previousElementSibling
+        .textContent
+    const body = e.target.parentElement.previousElementSibling.textContent
+
+    const data = {
+      id,
+      title,
+      body,
+    }
+
+    // Fill form with current post
+    ui.fillForm(data)
+  }
+
+  e.preventDefault()
 }
